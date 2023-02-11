43d64b51eb6f60e64dc5e9a2586fb932274aa421
lilian <1220795758@qq.com>
2023-02-11 17:48:47 +0800

    fix: 去除koa-body配置

--- src/middleware/initApp.js
+++ src/middleware/initApp.js
@@ -3,20 +3,21 @@ const logger = require("koa-logger")
 const static = require("koa-static")
 const { koaBody } = require("koa-body")
 
-const uploadConfig = {
-  multipart: true, // 支持文件上传
-  encoding: "gzip",
-  formidable: {
-    uploadDir: path.join(__dirname, "../../public/upload"), // 设置文件上传目录
-    keepExtensions: true, // 保持文件的后缀
-    maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
-    onFileBegin: (name, file) => {
-      // 文件上传前的设置
-      // console.log(`name: ${name}`)
-      // console.log(file)
-    }
-  }
-}
+// koa-body文件上传配置
+// const uploadConfig = {
+//   multipart: true, // 支持文件上传
+//   encoding: "gzip",
+//   formidable: {
+//     uploadDir: path.join(__dirname, "../../public/upload"), // 设置文件上传目录
+//     keepExtensions: true, // 保持文件的后缀
+//     maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+//     onFileBegin: (name, file) => {
+//       // 文件上传前的设置
+//       // console.log(`name: ${name}`)
+//       // console.log(file)
+//     }
+//   }
+// }
 
 // 打印日志、请求体解析、静态资源托管
 const initApp = (app) => {
